c082be44f20ec5e6a2dfe3dbf26e8fe35b07c90d
송영모 <mooyoung2309@gmail.com>
2022-03-16 13:16:49 +0900

    feat: create app

---
+++ moeum/moeum/moeumApp.swift
@@ -0,0 +1,17 @@
+//
+//  moeumApp.swift
+//  moeum
+//
+//  Created by 송영모 on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct moeumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
