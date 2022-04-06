8df3cfbd5b2e858a9000a1165fe3e0551c2a773c
송영모 <mooyoung2309@gmail.com>
2022-04-06 15:35:49 +0900

    feat: add trade record

---
+++ moeum/moeum/Models/Trade.swift
@@ -0,0 +1,20 @@
+//
+//  Trade.swift
+//  moeum
+//
+//  Created by 송영모 on 2022/04/06.
+//
+
+import Foundation
+
+struct TradeRecord {
+    var tagName: String
+    var itemName: String
+    var buyPrice: Double
+    var sellPrice: Double
+    var buyCount: Double
+    var sellCount: Double
+    var buyDate: Date
+    var sellDate: Date
+    var memo: String
+}
